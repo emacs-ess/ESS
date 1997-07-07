55a91528ce9c9371759cfc53801aaaac9d7f94fd
AJ Rossini <blindglobe@gmail.com>
1997-07-07 21:39:05 +0000

    can't set variables in initial call!
    git-svn-id: https://svn.r-project.org/ESS/trunk@122 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-r.el
+++ lisp/essd-r.el
@@ -5,9 +5,9 @@
 ;; Author: A.J. Rossini <rossini@stat.sc.edu>
 ;; Maintainer: A.J. Rossini <rossini@stat.sc.edu>
 ;; Created: 12 Jun 1997
-;; Modified: $Date: 1997/07/07 16:51:22 $
-;; Version: $Revision: 1.19 $
-;; RCS: $Id: essd-r.el,v 1.19 1997/07/07 16:51:22 rossini Exp $
+;; Modified: $Date: 1997/07/07 21:39:05 $
+;; Version: $Revision: 1.20 $
+;; RCS: $Id: essd-r.el,v 1.20 1997/07/07 21:39:05 rossini Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -32,6 +32,9 @@
 
 ;;;
 ;;: $Log: essd-r.el,v $
+;;: Revision 1.20  1997/07/07 21:39:05  rossini
+;;: can't set variables in initial call!
+;;:
 ;;: Revision 1.19  1997/07/07 16:51:22  rossini
 ;;: R2 -> R in debug-message.
 ;;:
@@ -110,7 +113,7 @@ New way to do it."
   (interactive)
   ;; Setup the needed vars
   (setq ess-customize-alist R-customize-alist) ; setq or setq-default?
-  (ess-set-vars-default ess-customize-alist (current-buffer))
+  ;;  (ess-set-vars-default ess-customize-alist (current-buffer))
   ;; debug, only
   (message "(R): ess-proc-prefix=%s , buf=%s"
 	   ess-proc-prefix (current-buffer))
