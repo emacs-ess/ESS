cb922413a7c86e6651a5a39f4c8ecb39b65fa1d0
AJ Rossini <blindglobe@gmail.com>
1997-07-17 19:24:22 +0000

    stuff.
    git-svn-id: https://svn.r-project.org/ESS/trunk@138 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/ess.el
+++ lisp/ess.el
@@ -8,9 +8,9 @@
 ;; Author: Doug Bates, Ed Kademan, Frank Ritter, David Smith
 ;; Maintainers: A.J. Rossini <rossini@stat.sc.edu>
 ;; Created: October 14, 1991
-;; Modified: $Date: 1997/07/17 19:12:28 $
-;; Version: $Revision: 1.40 $
-;; RCS: $Id: ess.el,v 1.40 1997/07/17 19:12:28 rossini Exp $
+;; Modified: $Date: 1997/07/17 19:24:22 $
+;; Version: $Revision: 1.41 $
+;; RCS: $Id: ess.el,v 1.41 1997/07/17 19:24:22 rossini Exp $
 ;; Lisp-dir-entry  : ess-mode|
 ;;                   K. Hornik, M. Maechler, A.J. Rossini|
 ;;                   rossini@stat.sc.edu|
@@ -111,6 +111,9 @@
 
 ;;
 ;; $Log: ess.el,v $
+;; Revision 1.41  1997/07/17 19:24:22  rossini
+;; stuff.
+;;
 ;; Revision 1.40  1997/07/17 19:12:28  rossini
 ;; *** empty log message ***
 ;;
@@ -1100,18 +1103,19 @@ This is SO UGLY.  But it'll work for now...
 the basic idea: (setq ---  (cdr (assq --- var-alist)))."
 
   (if buf (set-buffer buf))
+
   ;;(set-default ess-customize-alist var-alist)
-  (set ess-proc-prefix  (cdr (assq 'ess-proc-prefix var-alist)))
-  (set ess-version-running (cdr (assq 'ess-version-running var-alist)))
-  (set inferior-ess-program (cdr (assq 'inferior-ess-program var-alist)))
-  (set inferior-ess-objects-command (cdr (assq 'inferior-ess-objects-command var-alist)))
-  (set ess-help-sec-regex (cdr (assq 'ess-help-sec-regex var-alist)))
-  (set ess-help-sec-keys-alist (cdr (assq 'ess-help-sec-keys-alist var-alist)))
-  (set inferior-ess-help-command (cdr (assq 'inferior-ess-help-command var-alist)))
-  (set inferior-ess-exit-command (cdr (assq 'inferior-ess-exit-command var-alist)))
-  (set ess-loop-timeout (cdr (assq 'ess-loop-timeout var-alist)))
-  (set inferior-ess-primary-prompt (cdr (assq 'inferior-ess-primary-prompt var-alist)))
-  (set ess-history-file (concat "." ess-proc-prefix "history"))
+  (set 'ess-proc-prefix  (cdr (assq 'ess-proc-prefix var-alist)))
+  (set 'ess-version-running (cdr (assq 'ess-version-running var-alist)))
+  (set 'inferior-ess-program (cdr (assq 'inferior-ess-program var-alist)))
+  (set 'inferior-ess-objects-command (cdr (assq 'inferior-ess-objects-command var-alist)))
+  (set 'ess-help-sec-regex (cdr (assq 'ess-help-sec-regex var-alist)))
+  (set 'ess-help-sec-keys-alist (cdr (assq 'ess-help-sec-keys-alist var-alist)))
+  (set 'inferior-ess-help-command (cdr (assq 'inferior-ess-help-command var-alist)))
+  (set 'inferior-ess-exit-command (cdr (assq 'inferior-ess-exit-command var-alist)))
+  (set 'ess-loop-timeout (cdr (assq 'ess-loop-timeout var-alist)))
+  (set 'inferior-ess-primary-prompt (cdr (assq 'inferior-ess-primary-prompt var-alist)))
+  (set 'ess-history-file (concat "." ess-proc-prefix "history"))
 
   (ess-write-to-dribble-buffer 
     (format "(ess-set-vars): ess-proc-prefix=%s buf=%s \n"
