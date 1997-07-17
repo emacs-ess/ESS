8ec326cffef3f59f8b7e79dd904864cdb4e858c5
AJ Rossini <blindglobe@gmail.com>
1997-07-17 18:20:42 +0000

    replaced message with write to dribble buffer.
    git-svn-id: https://svn.r-project.org/ESS/trunk@126 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-r.el
+++ lisp/essd-r.el
@@ -5,9 +5,9 @@
 ;; Author: A.J. Rossini <rossini@stat.sc.edu>
 ;; Maintainer: A.J. Rossini <rossini@stat.sc.edu>
 ;; Created: 12 Jun 1997
-;; Modified: $Date: 1997/07/07 21:39:05 $
-;; Version: $Revision: 1.20 $
-;; RCS: $Id: essd-r.el,v 1.20 1997/07/07 21:39:05 rossini Exp $
+;; Modified: $Date: 1997/07/17 18:20:42 $
+;; Version: $Revision: 1.21 $
+;; RCS: $Id: essd-r.el,v 1.21 1997/07/17 18:20:42 rossini Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -32,6 +32,9 @@
 
 ;;;
 ;;: $Log: essd-r.el,v $
+;;: Revision 1.21  1997/07/17 18:20:42  rossini
+;;: replaced message with write to dribble buffer.
+;;:
 ;;: Revision 1.20  1997/07/07 21:39:05  rossini
 ;;: can't set variables in initial call!
 ;;:
@@ -113,10 +116,8 @@ New way to do it."
   (interactive)
   ;; Setup the needed vars
   (setq ess-customize-alist R-customize-alist) ; setq or setq-default?
-  ;;  (ess-set-vars-default ess-customize-alist (current-buffer))
-  ;; debug, only
-  (message "(R): ess-proc-prefix=%s , buf=%s"
-	   ess-proc-prefix (current-buffer))
+  (ess-write-to-dribble-buffer (format "(R): ess-proc-prefix=%s , buf=%s"
+				       ess-proc-prefix (current-buffer)))
   ;; now run...
   (inferior-ess))
 
