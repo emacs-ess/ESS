23934a5f9b8c1e1a069056b25ce5d3b463b75657
AJ Rossini <blindglobe@gmail.com>
1997-07-25 15:10:13 +0000

    merged old d-s4.el (RMH stuff) and DB's version, so that I remember that there is code to include!
    git-svn-id: https://svn.r-project.org/ESS/trunk@157 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-s4.el
+++ lisp/essd-s4.el
@@ -1,32 +1,3 @@
-X-From-Line: bates@stat.wisc.edu Wed Jul 23 13:11:13 1997
-Received: from franz.stat.wisc.edu (root@franz.stat.wisc.edu [128.105.5.95]) by milo.math.sc.edu (8.6.12/8.6.12) with ESMTP id NAA26959 for <rossini@stat.sc.edu>; Wed, 23 Jul 1997 13:11:12 -0400
-Received: by franz.stat.wisc.edu
-	id m0wr4wI-000hn9C
-	(Debian Smail-3.2 1996-Jul-4 #2); Wed, 23 Jul 1997 12:11:02 -0500 (CDT)
-Sender: bates@stat.wisc.edu (Douglas Bates)
-Sender: bates@franz.stat.wisc.edu
-To: Anthony Rossini <rossini@stat.sc.edu>
-Cc: jmc@research.bell-labs.com
-Subject: Re: S-mode-4.9-b8
-References: <m0wr3RC-000hn9C@franz.stat.wisc.edu> <wncsox5og77.fsf@urn.math.sc.edu>
-From: Douglas Bates <bates@stat.wisc.edu>
-Date: 23 Jul 1997 12:11:02 -0500
-In-Reply-To: Anthony Rossini's message of 23 Jul 1997 13:01:48 -0400
-Message-ID: <6r204pg0d5.fsf@franz.stat.wisc.edu>
-X-Mailer: Gnus v5.4.37/XEmacs 19.15
-Xref: urn.math.sc.edu mail.S-mode:50
-Lines: 122
-
-I started with a vanilla essd-s4.el as below.  Essentially I started
-from essd-s+3.el rather than going back through all the older
-modifications. It is working enough to get me going.  I haven't
-exercised it to any great extent.
-
-As it stands it will do object name completion and call up help pages
-so it is a start.  I'm sure there are many things that can be done to
-enhance it but the new modular structure should make those things much
-easier to accomplish.
-
 ;;; essd-s4.el --- S4 customization
 
 ;; Copyright (C) 1997 A.J. Rossini
@@ -34,9 +5,9 @@ easier to accomplish.
 ;; Author: A.J. Rossini <rossini@stat.sc.edu>
 ;; Maintainer: A.J. Rossini <rossini@stat.sc.edu>
 ;; Created: 12 Jun 1997
-;; Modified: $Date: 1997/07/25 14:57:53 $
-;; Version: $Revision: 1.3 $
-;; RCS: $Id: essd-s4.el,v 1.3 1997/07/25 14:57:53 rossini Exp $
+;; Modified: $Date: 1997/07/25 15:10:13 $
+;; Version: $Revision: 1.4 $
+;; RCS: $Id: essd-s4.el,v 1.4 1997/07/25 15:10:13 rossini Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -57,11 +28,17 @@ easier to accomplish.
 ;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Commentary:
+;;; DB contributed the changes from essd-s+3.el to
+;;; essd-s4.el. (removed the old ugly approach).
 ;;; This file defines S4 customizations for ess-mode.  Lots of thanks
 ;;; to RMH and JMC for code and suggestions
 
 ;;;
 ;;; $Log: essd-s4.el,v $
+;;; Revision 1.4  1997/07/25 15:10:13  rossini
+;;; merged old d-s4.el (RMH stuff) and DB's version, so that I remember
+;;; that there is code to include!
+;;;
 ;;; Revision 1.3  1997/07/25 14:57:53  rossini
 ;;; based on suggestions from Doug Bates.
 ;;;
@@ -117,6 +94,209 @@ New way to do it."
 	   ess-proc-prefix
 	   (current-buffer)))
   (inferior-ess))
+
+
+
+
+;; From RMH:    ALL THIS SHOULD BE INCORPORATED BY 5.0!
+
+;;; s4.el startup file
+;;; Richard M. Heiberger
+;;; rmh@astro.ocis.temple.edu
+;;
+;;(load "S")
+;;(setq inferior-S-program "/disk05/s4/betaJun96/S")
+;;(setq S-plus nil)                            ;; needed for non S-plus
+;;(add-to-list 'load-path "/disk05/s4/betaJun96") ;; S-namedb.el is here
+;;(S)
+;;(load-file "/disk05/s4/betaJun96/library/emacs/S-modeadds.el") ;; must come after (S)
+;;
+;;
+;;;;; S4 __Help, no longer S3 .Help
+;;(load "S-help")
+;;                                             ;; Must follow S-help
+;;; S-help.file line 270
+;;(defun S-get-help-files-list nil
+;;  (mapcar 'list
+;;	  (apply 'append
+;;		 (mapcar '(lambda (dirname)
+;;			    (if (file-directory-p dirname) 
+;;				(directory-files dirname)))
+;;			 (mapcar '(lambda (str) (concat str "/__Help"))
+;;				 (S-search-list))))))
+;;
+;;
+;;;;; additional font-lock-keywords for S4
+;;
+;;;;*;; based on S-inf.el line 107
+;;;;(add-to-list 'S-inf-font-lock-keywords
+;;;;	     '("\\<\\(^Problem\\|^Warning\\|^Error\\|Debug ?\\|Browsing in frame of\\|Local Variables\\)\\>" . font-lock-reference-face) ; S-inf problems
+;;;;)
+;;;;(add-to-list 'S-inf-font-lock-keywords
+;;;; '("^R>" . font-lock-keyword-face)	; debug prompt
+;;;;)
+;;(inferior-S-mode)
+;;
+;;; S-inf.el line 150
+;;(setq inferior-S-search-list-command "searchPaths()\n")
+;;
+;;;; fontify S-transcript-mode
+;;;; overwrites S-trans.el lines 60-69
+;;;;(setq S-trans-font-lock-keywords S-inf-font-lock-keywords)
+;;
+;;(load "S-mode")
+;;                                             ;; Must follow S-mode
+;;;;*;; based on S-mode.el line 219
+;;(add-to-list 'S-mode-font-lock-keywords
+;;	     '("\\<\\(setGeneric\\|removeGeneric\\|setMethod\\|unsetMethod\\|setReplaceGeneric\\|setReplaceMethod\\|standardGeneric\\|setIs\\|setClass\\|representation\\)\\>" . font-lock-function-name-face)  ; S4 method functions
+;;)
+;;
+;;
+;;
+;;;;; fix to S-load-file to make C-c C-l work with S4
+;;
+;;;When a file sourced into S4 by C-c C-l has a syntax error
+;;;without the following changes, the system
+;;;freezes until it is released with ^G.  The reason is that the error
+;;;messages, including the `Debug ?' request, go to the *S-errors*
+;;;buffer.  The *S-errors* buffer is not switched to, and couldn't accept
+;;;a response if it were.
+;;;
+;;;The fix requires three modification to S-inf.el and two to S-mode.el.
+;;;The correction to S-check-source noted in smode.cmt is also necessary.
+;;;
+;;
+;;; S-inf.el line 92  NEW variable
+;;(defvar inferior-S-debug-prompt "Debug \\? (y|n): "
+;; "The expression S uses to offer to initiate debug tracing.")
+;;
+;;; S-inf.el line 458
+;;(defun inferior-S-wait-for-prompt ()
+;;  "Wait until the S process is ready for input."
+;;  (let* ((cbuffer (current-buffer))
+;;         (sprocess (get-S-process S-current-process-name))
+;;         (sbuffer (process-buffer sprocess))
+;;         r
+;;	 (timeout 0))
+;;    (set-buffer sbuffer)
+;;    (while (progn
+;;	     (if (not (eq (process-status sprocess) 'run))
+;;		 (S-error "S process has died unexpectedly.")
+;;	       (if (> (setq timeout (1+ timeout)) S-loop-timeout)
+;;		   (S-error "Timeout waiting for prompt. Check inferior-S-prompt or S-loop-timeout."))
+;;	       (accept-process-output)
+;;	       (goto-char (point-max))
+;;(setq end (point))
+;;	       (beginning-of-line)
+;;(setq e (buffer-substring (point) end))
+;;(if (equal e inferior-S-debug-prompt)
+;;    (S-error "Debug prompt"))
+;;	       (setq r (looking-at inferior-S-prompt))
+;;	       (not (or r (looking-at ".*\\?\\s *"))))))
+;;    (goto-char (point-max))
+;;    (set-buffer cbuffer)
+;;    (symbol-value r)))
+;;
+;;
+;;
+;;; S-mode.el line 204
+;;(setq S-dump-error-re "Problem")
+;;
+;;;; S-mode.el line 655
+;;(defun S-parse-errors (showerr)
+;;  "Jump to error in last loaded S source file.
+;;With prefix argument, only shows the errors S reported."
+;;  (interactive "P")
+;;  (S-make-buffer-current)
+;;  (let ((errbuff (get-buffer S-error-buffer-name)))
+;;    (if (not errbuff)
+;;        (error "You need to do a load first!")
+;;      (set-buffer errbuff)
+;;      (goto-char (point-max))
+;;      (if
+;;          (re-search-backward ", file \"" nil t)
+;;	  (let* ((beg-pos (progn (re-search-forward "\"" nil t) (point)))
+;;		 (end-pos (progn (re-search-forward "\"" nil t) (- (point) 1)))
+;;		 (filename (buffer-substring beg-pos end-pos))
+;;                 (fbuffer (get-file-buffer filename))
+;;                 (linenum (string-to-int
+;;			   (progn (re-search-backward "," nil t)
+;;				  (current-word))))
+;;		 (end-pos (point))
+;;                 (beg-pos (progn (goto-char (point-min))
+;;				 (re-search-forward ":" nil t)
+;;				 (1+ (point))))
+;;                 (errmess (buffer-substring beg-pos end-pos))
+;;		 )
+;;            (if showerr
+;;                  (S-display-temp-buffer errbuff)
+;;              (if fbuffer nil
+;;                (setq fbuffer (find-file-noselect filename))
+;;                (save-excursion
+;;                  (set-buffer fbuffer)
+;;                  (S-mode)))
+;;              (pop-to-buffer fbuffer)
+;;              (goto-line linenum))
+;;            (princ errmess t))
+;;        (message "Not a syntax error.")
+;;        (S-display-temp-buffer errbuff)))))
+;;
+;;
+;;
+;;;; S-inf.el line 584
+;;(defun S-prompt-wait (proc &optional start-of-output)
+;;  "Wait for a prompt to appear at BOL of current buffer
+;;PROC is the S process. Does not change point"
+;;  (if start-of-output nil (setq start-of-output (point-min)))
+;;  (save-excursion
+;;    (while (progn
+;;	     ;; get output if there is some ready
+;;	     (accept-process-output proc 0 500) 
+;;	     (goto-char (marker-position (process-mark proc)))
+;;	     (beginning-of-line)
+;;
+;;	     (if (re-search-forward inferior-S-debug-prompt nil t)
+;;		 (if (equal (get-buffer S-error-buffer-name)
+;;			    (get-buffer S-error-buffer-name))
+;;		     (let* ((sprocess (get-S-process S-current-process-name))
+;;			    (sbuffer (process-buffer sprocess)))
+;;		       (set-buffer sbuffer)
+;;		       (process-send-string sprocess "n\n")
+;;		       (accept-process-output sprocess)
+;;		       (beginning-of-line); delete inferior-S-debug-prompt
+;;		       (kill-line)
+;;		       (insert "> ")))
+;;
+;;	     (if (< (point) start-of-output) (goto-char start-of-output))
+;;	     (not (looking-at inferior-S-primary-prompt)))))))
+;;
+
+
+
+ ; Provide package
+
+(provide 'essd-s4)
+
+ ; Local variables section
+
+;;; This file is automatically placed in Outline minor mode.
+;;; The file is structured as follows:
+;;; Chapters:     ^L ;
+;;; Sections:    ;;*;;
+;;; Subsections: ;;;*;;;
+;;; Components:  defuns, defvars, defconsts
+;;;              Random code beginning with a ;;;;* comment
+
+;;; Local variables:
+;;; mode: emacs-lisp
+;;; outline-minor-mode: nil
+;;; mode: outline-minor
+;;; outline-regexp: "\^L\\|\\`;\\|;;\\*\\|;;;\\*\\|(def[cvu]\\|(setq\\|;;;;\\*"
+;;; End:
+
+;;; essd-s4.el ends here
+
+
 
 
  ; Provide package
