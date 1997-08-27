27cc3788b54ed098882b3dce5bf3386b551d9e1b
AJ Rossini <blindglobe@gmail.com>
1997-08-27 18:44:20 +0000

    removed log, self-referential statements.
    git-svn-id: https://svn.r-project.org/ESS/trunk@243 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-xls.el
+++ lisp/essd-xls.el
@@ -5,9 +5,9 @@
 ;; Author: A.J. Rossini <rossini@stat.sc.edu>
 ;; Maintainer: A.J. Rossini <rossini@stat.sc.edu>
 ;; Created: 12 Jun 1997
-;; Modified: $Date: 1997/08/26 22:43:50 $
-;; Version: $Revision: 1.21 $
-;; RCS: $Id: essd-xls.el,v 1.21 1997/08/26 22:43:50 rossini Exp $
+;; Modified: $Date: 1997/08/27 18:44:20 $
+;; Version: $Revision: 1.22 $
+;; RCS: $Id: essd-xls.el,v 1.22 1997/08/27 18:44:20 rossini Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -30,58 +30,6 @@
 ;;; Commentary:
 ;;; This file defines all the Splus 3.x customizations for ess-mode.
 
-;;;
-;;: $Log: essd-xls.el,v $
-;;: Revision 1.21  1997/08/26 22:43:50  rossini
-;;: *** empty log message ***
-;;:
-;;: Revision 1.20  1997/08/25 14:31:04  rossini
-;;: *** empty log message ***
-;;:
-;;: Revision 1.19  1997/07/31 11:52:39  rossini
-;;: changed to reflect current.
-;;:
-;;: Revision 1.18  1997/07/31 11:32:53  rossini
-;;: added program name.
-;;:
-;;: Revision 1.17  1997/07/17 20:40:50  rossini
-;;: cleaned up for release.
-;;:
-;;: Revision 1.16  1997/07/17 18:31:21  rossini
-;;: stuff
-;;:
-;;: Revision 1.15  1997/07/17 18:30:58  rossini
-;;: XLS, not R!
-;;:
-;;: Revision 1.14  1997/07/17 18:20:23  rossini
-;;: replaced message with a write-to-dribble-buffer.
-;;:
-;;: Revision 1.13  1997/07/07 21:39:29  rossini
-;;: can't set variables in initial call!!
-;;:
-;;: Revision 1.12  1997/07/07 16:57:50  rossini
-;;: need to use program name, not external variables...
-;;:
-;;: Revision 1.11  1997/07/07 16:53:46  rossini
-;;: setup new-style, corrected alist.
-;;:
-;;: Revision 1.10  1997/07/07 16:51:45  rossini
-;;: added new-style language variable setup.
-;;:
-;;: Revision 1.9  1997/07/03 13:36:16  rossini
-;;: added inferior-ess autoload.
-;;:
-;;: Revision 1.8  1997/07/02 16:21:10  rossini
-;;: removed hooks.
-;;:
-;;: Revision 1.7  1997/07/02 16:15:41  rossini
-;;: moved variables to XLS defun.
-;;:
-;;: Revision 1.6  1997/06/14 23:14:17  rossini
-;;: finally setup.
-;;:
-;;;
-
 ;;; Autoloads:
 
 (autoload 'inferior-ess "ess-inf" "Run an ESS process")
@@ -89,8 +37,7 @@
 ;;; Code:
 
 (defvar XLS-customize-alist
-  '((ess-customize-alist           .  XLS-customize-alist )
-    (ess-language                  .  "XLS"               )
+  '((ess-language                  .  "XLS"               )
     (ess-dialect                   .  "XLS"              )
     (ess-loop-timeout              .  10000               )
     (ess-object-name-db-file       .  "ess-xls-namedb.el" )
@@ -102,8 +49,7 @@
     (inferior-ess-objects-command  .  "(variables)\n"     )
     (inferior-ess-exit-command     .  "(exit)\n"          )
     (inferior-ess-start-file       . "~/.ess-XLS")
-    (inferior-ess-start-args       . nil)
-    )
+    (inferior-ess-start-args       . nil))
   "Variables to customize for XLS")
 
 
