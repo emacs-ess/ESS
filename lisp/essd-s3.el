2d8547a4ad661a7dfa3d07cff6fecd1fc692baae
AJ Rossini <blindglobe@gmail.com>
1997-09-02 20:12:01 +0000

    *** empty log message ***
    git-svn-id: https://svn.r-project.org/ESS/trunk@288 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-s3.el
+++ lisp/essd-s3.el
@@ -5,9 +5,9 @@
 ;; Author: A.J. Rossini <rossini@stat.sc.edu>
 ;; Maintainer: A.J. Rossini <rossini@stat.sc.edu>
 ;; Created: 12 Jun 1997
-;; Modified: $Date: 1997/08/26 22:54:23 $
-;; Version: $Revision: 1.7 $
-;; RCS: $Id: essd-s3.el,v 1.7 1997/08/26 22:54:23 rossini Exp $
+;; Modified: $Date: 1997/09/02 20:12:01 $
+;; Version: $Revision: 1.8 $
+;; RCS: $Id: essd-s3.el,v 1.8 1997/09/02 20:12:01 rossini Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -28,82 +28,13 @@
 ;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Commentary:
-;;; This file defines all the Splus 3.x customizations for ess-mode.
-
-;;;
-;;: $Log: essd-s3.el,v $
-;;: Revision 1.7  1997/08/26 22:54:23  rossini
-;;: *** empty log message ***
-;;:
-;;: Revision 1.6  1997/08/25 20:50:43  rossini
-;;: MM's changes.
-;;:
-;;: Revision 1.5  1997/08/25 14:31:04  rossini
-;;: *** empty log message ***
-;;:
-;;: Revision 1.4  1997/07/30 13:14:22  rossini
-;;: vars back in for program name.
-;;:
-;;: Revision 1.3  1997/07/30 13:10:36  rossini
-;;: header line corrected.
-;;:
-;;: Revision 1.2  1997/07/28 13:00:09  rossini
-;;: provide the right package!
-;;:
-;;: Revision 1.1  1997/07/24 12:34:34  rossini
-;;: Initial revision
-;;:
-;;: Revision 1.14  1997/07/17 20:57:15  rossini
-;;: works, now..
-;;:
-;;: Revision 1.13  1997/07/17 20:51:31  rossini
-;;: stuff
-;;:
-;;: Revision 1.12  1997/07/17 20:49:21  rossini
-;;: new version, take 1.
-;;:
-;;: Revision 1.11  1997/06/22 23:13:14  rossini
-;;: removed S-inf... variable.  Whoops.
-;;:
-;;: Revision 1.10  1997/06/19 21:17:02  rossini
-;;: added font-lock-keywords, ala RMH.
-;;:
-;;: Revision 1.9  1997/06/15 08:42:53  rossini
-;;: setq-> setq-default.  I think this is correct!@
-;;:
-;;: Revision 1.8  1997/06/15 08:18:10  rossini
-;;: added autoload for inferior-ess
-;;:
-;;: Revision 1.7  1997/06/15 07:11:10  rossini
-;;: need to provide the actual file name.
-;;:
-;;: Revision 1.6  1997/06/14 23:12:08  rossini
-;;: Finally set up properly.
-;;:
-;;;
+;;; This file defines all the S 3 customizations for ess-mode.
 
 ;;; Autoloads:
 
 (autoload 'inferior-ess "ess-inf" "Run an ESS process")
 
 ;;; Code:
-
-(defvar S3-help-sec-keys-alist
-  '((?a . "ARGUMENTS:")
-    (?b . "BACKGROUND:")
-    (?B . "BUGS:")
-    (?d . "DETAILS:")
-    (?D . "DESCRIPTION:")
-    (?e . "EXAMPLES:")
-    (?n . "NOTE:")
-;;    (?o . "OPTIONAL ARGUMENTS:")
-;;    (?r . "REQUIRED ARGUMENTS:")
-    (?R . "REFERENCES:")
-    (?S . "SEE ALSO:")
-    (?s . "SIDE EFFECTS:")
-    (?u . "USAGE:")
-    (?v . "VALUE:"))
-  "Help section keys for display.")
 
 (defvar S3-customize-alist
   '((ess-language                  . "S")
