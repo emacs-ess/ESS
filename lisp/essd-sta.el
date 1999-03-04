5aee9af1466f62658d5800c546762da60ced95c5
AJ Rossini <blindglobe@gmail.com>
1999-03-04 22:43:14 +0000

    commented out ESS-help for Stata, for compiling, initial testing.
    git-svn-id: https://svn.r-project.org/ESS/trunk@981 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-sta.el
+++ lisp/essd-sta.el
@@ -5,9 +5,9 @@
 ;; Author: A.J. Rossini <rossini@biostat.washington.edu>
 ;; Maintainer: A.J. Rossini <rossini@biostat.washington.edu>
 ;; Created: 9 Sep 1998
-;; Modified: $Date: 1999/02/22 23:01:14 $
-;; Version: $Revision: 1.4 $
-;; RCS: $Id: essd-sta.el,v 1.4 1999/02/22 23:01:14 rossini Exp $
+;; Modified: $Date: 1999/03/04 22:43:14 $
+;; Version: $Revision: 1.5 $
+;; RCS: $Id: essd-sta.el,v 1.5 1999/03/04 22:43:14 rossini Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -54,8 +54,8 @@
     (ess-mode-editing-alist        . STA-editing-alist)
     (ess-mode-syntax-table         . STA-syntax-table)
     (ess-mode-edit                 . 'STA-mode)
-    (ess-help-sec-regex            . ess-help-STA-sec-regex)
-    (ess-help-sec-keys-alist       . STA-help-sec-keys-alist)
+    (ess-help-sec-regex            . nil) ;ess-help-STA-sec-regex)
+    (ess-help-sec-keys-alist       . nil) ;STA-help-sec-keys-alist)
     (ess-loop-timeout              . 100000 )
     (ess-object-name-db-file       . "ess-sta-namedb.el" )
     (ess-retr-last. evalue-command    . 'nil)
