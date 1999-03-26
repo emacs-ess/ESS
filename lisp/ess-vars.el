7a592d40f449d02a3e49cfe1a418835e611b59bc
Martin Maechler <maechler@stat.math.ethz.ch>
1999-03-26 08:00:29 +0000

    5.1.5 (at least according to ANNOUNCE/Makefile....
    git-svn-id: https://svn.r-project.org/ESS/trunk@1049 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/ess-vars.el
+++ lisp/ess-vars.el
@@ -6,9 +6,9 @@
 ;; Author: A.J. Rossini <rossini@biostat.washington.edu>
 ;; Maintainer: A.J. Rossini <rossini@biostat.washington.edu>
 ;; Created: 25 July 1997
-;; Modified: $Date: 1999/03/17 17:42:45 $
-;; Version: $Revision: 5.18 $
-;; RCS: $Id: ess-vars.el,v 5.18 1999/03/17 17:42:45 rossini Exp $
+;; Modified: $Date: 1999/03/26 08:00:29 $
+;; Version: $Revision: 5.19 $
+;; RCS: $Id: ess-vars.el,v 5.19 1999/03/26 08:00:29 maechler Exp $
 
 ;; Keywords: editing and process modes.
 
@@ -34,7 +34,7 @@
 
 ;;; Code:
 
-(defconst ess-version "5.1.4"
+(defconst ess-version "5.1.5"
   "Version of ESS currently loaded.")
 
 (defvar no-doc
