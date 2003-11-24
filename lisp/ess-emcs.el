d119e85bdc5b474e2f96271c0b569707938395be
Martin Maechler <maechler@stat.math.ethz.ch>
2003-11-24 10:28:21 +0000

    replace-regexp-in-string also needed for Emacs 20
    git-svn-id: https://svn.r-project.org/ESS/trunk@2574 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/ess-emcs.el
+++ lisp/ess-emcs.el
@@ -1,6 +1,6 @@
 ;;; ess-emcs.el --- simple determination of Emacs/XEmacs and version #.
 
-;; Copyright (C) 2000--2001 A.J. Rossini <rossini@u.washington.edu>,
+;; Copyright (C) 2000--2003 A.J. Rossini <rossini@u.washington.edu>,
 ;; R.M. Heiberger <rmh@surfer.sbm.temple.edu>,
 ;; Martin Maechler <maechler@stat.math.ethz.ch>,
 ;; Kurt Hornik <hornik@ci.tuwien.ac.at>, and
@@ -9,9 +9,9 @@
 ;; Author:  A.J. Rossini <rossini@biostat.washington.edu>
 ;; Maintainer: A.J. Rossini <rossini@biostat.washington.edu>
 ;; Created: 07 June 2000
-;; Modified: $Date: 2003/10/28 22:28:27 $
-;; Version: $Revision: 5.20 $
-;; RCS: $Id: ess-emcs.el,v 5.20 2003/10/28 22:28:27 rsparapa Exp $
+;; Modified: $Date: 2003/11/24 10:28:21 $
+;; Version: $Revision: 5.21 $
+;; RCS: $Id: ess-emcs.el,v 5.21 2003/11/24 10:28:21 maechler Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -93,11 +93,72 @@ Only a concern with earlier versions of Emacs.")
 	 (string-match "XEmacs\\|Lucid" emacs-version))
     (provide 'xemacs))
 
-;; XEmacs 21.x need this
+;; XEmacs 21.x and Emacs 20.x need this
 (if (not (fboundp 'replace-regexp-in-string))
-(defun replace-regexp-in-string(regexp replace string)
-"Mimic GNU Emacs function replace-regexp-in-string with XEmacs' replace-in-string"
-(replace-in-string string regexp replace)))
+    (if (featurep 'xemacs)
+	(defun replace-regexp-in-string(regexp replace string)
+	  "Mimic GNU Emacs function replace-regexp-in-string with XEmacs' replace-in-string"
+	  (replace-in-string string regexp replace))
+
+      ;; GNU emacs <= 20 -- take Emacs' 21(.3)'s definition:
+      (defun replace-regexp-in-string (regexp rep string &optional
+					      fixedcase literal subexp start)
+	"Replace all matches for REGEXP with REP in STRING.
+
+Return a new string containing the replacements.
+
+Optional arguments FIXEDCASE, LITERAL and SUBEXP are like the
+arguments with the same names of function `replace-match'.  If START
+is non-nil, start replacements at that index in STRING.
+
+REP is either a string used as the NEWTEXT arg of `replace-match' or a
+function.  If it is a function it is applied to each match to generate
+the replacement passed to `replace-match'; the match-data at this
+point are such that match 0 is the function's argument.
+
+To replace only the first match (if any), make REGEXP match up to \\'
+and replace a sub-expression, e.g.
+  (replace-regexp-in-string \"\\(foo\\).*\\'\" \"bar\" \" foo foo\" nil nil 1)
+    => \" bar foo\"
+"
+
+	;; To avoid excessive consing from multiple matches in long strings,
+	;; don't just call `replace-match' continually.  Walk down the
+	;; string looking for matches of REGEXP and building up a (reversed)
+	;; list MATCHES.  This comprises segments of STRING which weren't
+	;; matched interspersed with replacements for segments that were.
+	;; [For a `large' number of replacments it's more efficient to
+	;; operate in a temporary buffer; we can't tell from the function's
+	;; args whether to choose the buffer-based implementation, though it
+	;; might be reasonable to do so for long enough STRING.]
+	(let ((l (length string))
+	      (start (or start 0))
+	      matches str mb me)
+	  (save-match-data
+	    (while (and (< start l) (string-match regexp string start))
+	      (setq mb (match-beginning 0)
+		    me (match-end 0))
+	      ;; If we matched the empty string, make sure we advance by one char
+	      (when (= me mb) (setq me (min l (1+ mb))))
+	      ;; Generate a replacement for the matched substring.
+	      ;; Operate only on the substring to minimize string consing.
+	      ;; Set up match data for the substring for replacement;
+	      ;; presumably this is likely to be faster than munging the
+	      ;; match data directly in Lisp.
+	      (string-match regexp (setq str (substring string mb me)))
+	      (setq matches
+		    (cons (replace-match (if (stringp rep)
+					     rep
+					   (funcall rep (match-string 0 str)))
+					 fixedcase literal str subexp)
+			  (cons (substring string start mb) ; unmatched prefix
+				matches)))
+	      (setq start me))
+	    ;; Reconstruct a string from the pieces.
+	    (setq matches (cons (substring string start l) matches)) ; leftover
+	    (apply #'concat (nreverse matches)))))
+      )
+)
 
 (if (not (fboundp 'w32-using-nt))
 (defun w32-using-nt ()
@@ -108,7 +169,7 @@ Only a concern with earlier versions of Emacs.")
     (and (= emacs-major-version 21) (>= emacs-minor-version 4))))
   (defun ess-xemacs-insert-glyph (gl)
      "Insert a glyph at the left edge of point."
-     (let ( (prop 'myimage)        ;; myimage is an arbitrary name, chosen 
+     (let ( (prop 'myimage)        ;; myimage is an arbitrary name, chosen
                                    ;; to (hopefully) not conflict with any
                                    ;; other properties.  Change it if
                                    ;; necessary.
