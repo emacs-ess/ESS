c1a65b21511d628ee5cc61ab3fdf19de6f4cceab
Rodney Sparapani <rsparapa@mcw.edu>
2004-05-14 22:02:27 +0000

    committing god-awful definition of ess-etc-directory
    git-svn-id: https://svn.r-project.org/ESS/trunk@2780 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/ess-site.el
+++ lisp/ess-site.el
@@ -8,9 +8,9 @@
 ;; Maintainer: A.J. Rossini <rossini@u.washington.edu>, 
 ;;             Martin Maechler <maechler@stat.math.ethz.ch>
 ;; Created: 12 Nov 1993
-;; Modified: $Date: 2004/05/13 12:10:25 $
-;; Version: $Revision: 5.103 $
-;; RCS: $Id: ess-site.el,v 5.103 2004/05/13 12:10:25 stephen Exp $
+;; Modified: $Date: 2004/05/14 22:02:27 $
+;; Version: $Revision: 5.104 $
+;; RCS: $Id: ess-site.el,v 5.104 2004/05/14 22:02:27 rsparapa Exp $
 ;;
 ;; Keywords: start up, configuration.
 
@@ -183,11 +183,29 @@ The extension, in a file name, is the part that follows the last `.'."
 ;; Maybe ess-etc-directory should not be defcustom, since its value
 ;; depends on ess-lisp-directory, and is needed by other modes that are
 ;; loaded before the custom code.
-(defvar ess-etc-directory
-  (expand-file-name (concat ess-lisp-directory "/../etc/"))
+(defvar ess-etc-directory nil
+;  (expand-file-name (concat ess-lisp-directory "/../etc/"))
   "*Location of the ESS etc/ directory.
 The ESS etc directory stores various auxillary files that are useful
 for ESS, such as icons.")
+
+(defvar ess-etc-directory-list '("/../etc" "/../etc/ess" "/../../etc/ess")
+  "*List of directories, relative to `ess-lisp-directory', to search for etc.")
+
+(while (and (listp ess-etc-directory-list) (consp ess-etc-directory-list))
+    (setq ess-etc-directory 
+	(expand-file-name (concat ess-lisp-directory 
+	    (car ess-etc-directory-list))))
+    (if (file-directory-p ess-etc-directory) 
+	(setq ess-etc-directory-list nil)
+	(setq ess-etc-directory nil)
+	(setq ess-etc-directory-list (cdr ess-etc-directory-list))
+	(if (null ess-etc-directory-list) (progn
+	    (beep 0) (beep 0) (message (concat
+	    "ERROR:ess-site.el:ess-etc-directory\n"
+	    "Relative to ess-lisp-directory\n"
+	    "At least one of ../etc, ../etc/ess, ../../etc/ess must exist!"))
+	    (sit-for 4)))))
 
 ;;; (1.2) Uncomment the following 4 lines to fix the infopath, if needed.
 ;;(defun add-info-path (newpath)
