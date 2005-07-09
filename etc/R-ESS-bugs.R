141544ae78f11f31f7f3520c0575b1863f4eed3e
Martin Maechler <maechler@stat.math.ethz.ch>
2005-07-09 21:20:32 +0000

    add "hard" examples for beginning-of-function
    git-svn-id: https://svn.r-project.org/ESS/trunk@3320 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- etc/R-ESS-bugs.R
+++ etc/R-ESS-bugs.R
@@ -23,7 +23,7 @@ a <- function(ch) {
 
 
 ### Here, the beginning of function is not found correctly, and hence
-###       all "ess-*-function" fail:
+###       all "ess-*-function" (C-M-a, C-M-e, ...) fail:
 
 setMeneric <-
   ## It is clearly allowed to have comments here.
@@ -43,4 +43,86 @@ setMeneric <-
         FALSE
     }
     "ABC"
+}
+
+## this has one line more before 'function' than "typically:"
+setMethod("[", signature(x = "dgTMatrix", i = "numeric", j = "missing",
+			 drop = "logical"),
+	  function (x, i, j, ..., drop) { ## select rows
+	      storage.mode(i) <- "integer"
+              xi <- x@i + 1:1 # 1-indexing
+              ## ...................
+	      if (drop && any(nd == 1)) drop(as(x,"matrix")) else x
+	  })
+
+"dimnames<-.data.frame" <- function(x, value) {
+    d <- dim(x)
+    if(!is.list(value) || length(value) != 2
+       || d[[1]] != length(value[[1]])
+       || d[[2]] != length(value[[2]]))
+	stop("invalid 'dimnames' given for data frame")
+    row.names(x) <- as.character(value[[1]]) # checks validity
+    names(x) <- as.character(value[[2]])
+    x
+}
+
+'[.foo' <- function(x, i, value)
+{
+###
+    y <- x
+    y[i] <- value
+    y
+}
+
+'[[.bar' <- function(x, i, value)
+{
+    ## bla bla bla
+    y <- as.foo(x) ; y[[i]] <- value
+    y
+}
+
+"[<-.foobar" <- function(x,i,j,value) {
+    ## just something
+    x
+}
+
+"names<-.foobar" <- function(x, value) {
+    ## just something else
+    x
+}
+
+"[<-.data.frame" <- function(x, i, j, value)
+{
+    nA <- nargs() # value is never missing, so 3 or 4.
+
+###..........
+
+    class(x) <- cl
+    x
+}
+
+"[[<-.data.frame"<- function(x, i, j, value)
+{
+    cl <- oldClass(x)
+    ## delete class: Version 3 idiom
+    ## to avoid any special methods for [[<-
+    class(x) <- NULL
+
+###...........
+
+    class(x) <- cl
+    x
+}
+
+
+"$<-.data.frame"<- function(x, i, value)
+{
+    cl <- oldClass(x)
+    ## delete class: Version 3 idiom
+    ## to avoid any special methods for [[<-
+
+###...........
+
+    class(x) <- cl
+    return(x)
 }
