e8a1047d9ed24d3dd3ee571fe5f204aff6e308bb
Stephen Eglen <sje30@cam.ac.uk>
2005-09-02 09:02:59 +0000

    (ess-r-versions): Moved this variable from ess-cust.el.  (As a custom variable, it could be set after ess-site had been loaded, which is the time when its value is used.  Hence customized values were ignored.)
    git-svn-id: https://svn.r-project.org/ESS/trunk@3359 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-r.el
+++ lisp/essd-r.el
@@ -87,6 +87,20 @@
    S-common-cust-alist)
   "Variables to customize for R")
 
+(defvar ess-r-versions '("R-1" "R-2" "R-devel" "R-patched")
+  "List of partial strings for versions of R to access within ESS.
+Each string specifies the start of a filename.  If a filename
+beginning with one of these strings is found on `exec-path', a M-x
+command for that version of R is made available.  For example, if the
+file \"R-1.8.1\" is found and this variable includes the string
+\"R-1\", a function called `M-x R-1.8.1' will be available to run that
+version of R.
+If duplicate versions of the same program are found (which happens if
+the same path is listed on `exec-path' more than once), they are
+ignored by calling `ess-uniq-list'.
+If you set this variable, you need to restart Emacs (and set this variable 
+before ess-site is loaded) for it to take effect.")
+
 ;;;### autoload
 (defun R (&optional start-args)
   "Call 'R', the 'GNU S' system from the R Foundation.
