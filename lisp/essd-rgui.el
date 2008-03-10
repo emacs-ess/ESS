ccdce5a8d103c23c4fce80a7e0e921fb7a157a63
Rich Heiberger <rmh@temple.edu>
2008-03-10 19:31:12 +0000

    new files to connect with Rgui on Windows
    git-svn-id: https://svn.r-project.org/ESS/trunk@3850 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/essd-rgui.el
+++ lisp/essd-rgui.el
@@ -1,3 +1,32 @@
+;;; essd-rgui.el --- Support for running Rgui on Windows as an
+;;; inferior Emacs process
+
+;; Copyright (C) 2008 A.J. Rossini, Richard M. Heiberger, Martin
+;;	Maechler, Kurt Hornik, Rodney Sparapani, and Stephen Eglen.
+
+;; Original Author:  Richard M. Heiberger <rmh@temple.edu>
+;; Created: 10 Mar 2008
+;; Maintainers: ESS-core <ESS-core@stat.math.ethz.ch>
+
+;; This file is part of ESS
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.	If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+
 ;In Rgui:
 ;> library(tcltk2)  ## >= 1.0-6
 ;> .ess.command <- function() source("c:/temp/ess-tempfile.R", echo=TRUE)
