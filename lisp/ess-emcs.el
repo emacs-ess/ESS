194d7eddb8770b60ecb6439f2a658358e829f3c9
Martin Maechler <maechler@stat.math.ethz.ch>
2008-11-17 08:18:26 +0000

    emacs-(major|minor)-version  haved existed since (GNU|X) Emacs 19.x
    git-svn-id: https://svn.r-project.org/ESS/trunk@3970 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/ess-emcs.el
+++ lisp/ess-emcs.el
@@ -34,22 +34,6 @@
 
 ;;; Code:
 
-;; Older versions of emacs did not have these variables
-;; (emacs-major-version and emacs-minor-version.)
-;; Let's define them if they're not around, since they make
-;; it much easier to conditionalize on the emacs version.
-
-(if (and (not (boundp 'emacs-major-version))
-	 (string-match "^[0-9]+" emacs-version))
-    (setq emacs-major-version
-	  (string-to-number (substring emacs-version
-				    (match-beginning 0) (match-end 0)))))
-(if (and (not (boundp 'emacs-minor-version))
-	 (string-match "^[0-9]+\\.\\([0-9]+\\)" emacs-version))
-    (setq emacs-minor-version
-	  (string-to-number (substring emacs-version
-				    (match-beginning 1) (match-end 1)))))
-
 ;;; Define a function to make it easier to check which version we're
 ;;; running.
 
