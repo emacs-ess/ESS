5ade9e986911441ad6ad91dfbc76197cb8fcdeff
Rodney Sparapani <rsparapa@mcw.edu>
2009-05-15 19:37:49 +0000

    * ess-eldoc.el/ess-roxygen.el/ess-toolbar.el: 	added GPLv2 or later header, etc.
    git-svn-id: https://svn.r-project.org/ESS/trunk@4108 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- lisp/ess-toolbar.el
+++ lisp/ess-toolbar.el
@@ -1,6 +1,6 @@
 ;;; ess-toolbar.el --- Support for a toolbar in ESS.
 
-;; Copyright (C) 1997--2004 A.J. Rossini, Rich M. Heiberger, Martin
+;; Copyright (C) 1997--2009 A.J. Rossini, Rich M. Heiberger, Martin
 ;;	Maechler, Kurt Hornik, Rodney Sparapani, and Stephen Eglen.
 
 ;; Original Author: Stephen Eglen
@@ -10,7 +10,19 @@
 
 ;; This file is part of ESS
 
-;; GPL --> see beginning of ./ess-site.el
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Commentary:
 
