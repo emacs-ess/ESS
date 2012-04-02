a9f4c46753b0e857d9cd2a10a348e1d23b7959c1
Vitalie Spinu <spinuvit@gmail.com>
2012-04-02 13:56:14 +0200

    added copyrigh to ess-julia.el

--- lisp/ess-julia.el
+++ lisp/ess-julia.el
@@ -1,6 +1,45 @@
+;; ess-julia.el --- ESS julia mode and inferior interaction
+;;
+;; Copyright (C) 2012 Vitalie Spinu.
+;;
+;; Filename: ess-tracebug.el
+;; Author: Vitalie Spinu (based on julia-mode.el form julia-lang project)
+;; Maintainer: Vitalie Spinu
+;; Created: 02-04-2012 (ESS 12.03)
+;; Keywords: ESS, julia
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+
+;; This file is *NOT* part of GNU Emacs.
+;; This file is part of ESS
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;  customise inferior-julia-program-name to point to your julia-release-basic
+;;  and start the inferior with M-x julia.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
 ;
-; Emacs mode for Julia
-;
+
 
 (defvar julia-mode-hook nil)
 
