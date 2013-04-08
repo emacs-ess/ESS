d96243f56a91a160613196cbcae09a4316fdbd52
Vitalie Spinu <spinuvit@gmail.com>
2013-04-08 17:48:50 +0200

    making ` a string delimiter in julia

--- lisp/ess-julia.el
+++ lisp/ess-julia.el
@@ -61,6 +61,7 @@
     ;(modify-syntax-entry ?\\ "." table)  ; \ is an operator outside quotes
     (modify-syntax-entry ?'  "." table)  ; character quote or transpose
     (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?` "\"" table)
     ;; (modify-syntax-entry ?\" "." table)
     (modify-syntax-entry ?? "." table)
     (modify-syntax-entry ?$ "." table)
