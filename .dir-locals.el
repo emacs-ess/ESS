d1462776559f21ea8dd83c5ec010ced1b0c6054b
Vitalie Spinu <spinuvit@gmail.com>
2013-08-11 12:25:19 +0000

    in emacs 24.3.5 codding cannot be specified in .dir-locals
    git-svn-id: https://svn.r-project.org/ESS/trunk@5703 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,6 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((emacs-lisp-mode
-  (coding . utf-8-unix)
+  ;; (coding . utf-8-unix) ;; emacs 24.3.5 gives "Coding cannot be specified by dir-locals"
   (indent-tabs-mode)
   (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark)))
