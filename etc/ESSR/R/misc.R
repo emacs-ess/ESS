68fed023bfd079cedfc1221dc6fdcd48d43044f1
Vitalie Spinu <spinuvit@gmail.com>
2013-11-14 04:25:25 +0000

    again ... adjust headtail to treat small objects nicely
        ... better had written it myself :(
    
    git-svn-id: https://svn.r-project.org/ESS/trunk@5887 0bbaf3bd-34e0-0310-bf65-c717079852d4

--- etc/ESSR/R/misc.R
+++ etc/ESSR/R/misc.R
@@ -17,30 +17,33 @@
 headtail <- function (x, hlength = 4, tlength = 4, digits = 2, ellipsis = TRUE)
 {
     if (is.data.frame(x) | is.matrix(x)) {
-        if (is.matrix(x))
-            x <- data.frame(unclass(x))
-        nvar <- dim(x)[2]
-        dots <- rep("...", nvar)
-        h <- data.frame(head(x, hlength))
-        t <- data.frame(tail(x, tlength))
-        for (i in 1:nvar) {
-            if (is.numeric(h[1, i])) {
-                h[i] <- round(h[i], digits)
-                t[i] <- round(t[i], digits)
+        if(nrow(x) <= tlength + hlength){
+            print(x)
+        }else{
+            if (is.matrix(x))
+                x <- data.frame(unclass(x))
+            nvar <- dim(x)[2]
+            dots <- rep("...", nvar)
+            h <- data.frame(head(x, hlength))
+            t <- data.frame(tail(x, tlength))
+            for (i in 1:nvar) {
+                if (is.numeric(h[1, i])) {
+                    h[i] <- round(h[i], digits)
+                    t[i] <- round(t[i], digits)
+                }
+                else {
+                    dots[i] <- NA
+                }
+            }
+            if (ellipsis) {
+                head.tail <- rbind(h, ... = dots, t)
             }
             else {
-                dots[i] <- NA
+                head.tail <- rbind(h, t)
             }
+            print(head.tail)
         }
-        if (ellipsis) {
-            head.tail <- rbind(h, ... = dots, t)
-        }
-        else {
-            head.tail <- rbind(h, t)
-        }
-        print(head.tail)
-    }
-    else {
+    } else {
         cat("head(", hlength, "):\n", sep = "")
         print(head(x, hlength))
         cat("\ntail(", tlength, "):\n", sep = "")
