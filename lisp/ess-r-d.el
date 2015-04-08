e23a8c0bcdd45ac93090dce3acc459a408369ccc
Martin Maechler <maechler@r-project.org>
2015-04-08 22:07:45 +0200

    fix 2 from merge conflicts

--- lisp/ess-r-d.el
+++ lisp/ess-r-d.el
@@ -604,14 +604,10 @@ by the code on Windows for finding the newest version of R."
   :type 'boolean)
 
 (defun ess-rterm-prefer-higher-bit ()
-<<<<<<< HEAD
-  "Filter out Rterms."
-=======
   "Optionally remove 32bit Rterms from being candidate for R-newest.
-Return the list of candidates for being R-newest.  Filtering is done 
+Return the list of candidates for being R-newest.  Filtering is done
 iff `ess-prefer-higher-bit' is non-nil.
 This is used only by Windows when running `ess-find-newest-R'."
->>>>>>> b4142eeabb39c0f837e7c70c32c469f8c342a9de
   (if ess-prefer-higher-bit
     ;; filter out 32 bit elements
     (let ((filtered
@@ -623,8 +619,8 @@ This is used only by Windows when running `ess-find-newest-R'."
           ess-rterm-version-paths
         filtered))
     ess-rterm-version-paths))
-  
-    
+
+
 (defun ess-find-newest-R ()
   "Find the newest version of R on the system.  Once the value is found,
 cache it in the variable `ess-newest-R' for future use as finding the
