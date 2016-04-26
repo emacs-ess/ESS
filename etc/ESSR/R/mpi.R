3475f7ccfeebbc36bee560d934accaa4208eb750
Vitalie Spinu <spinuvit@gmail.com>
2016-04-26 12:09:06 +0200

    Add mpi.R to ESSR

---
+++ etc/ESSR/R/mpi.R
@@ -0,0 +1,10 @@
+## simple Message Parsing Inerface
+
+.ess_mpi_send <- function(head, payload, ...){
+    payload <- sprintf(as.character(payload), ...)
+    cat(sprintf("%s%s", head, payload))
+}
+
+.ess_mpi_MSG <- function(msg, ...){
+    .ess_mpi_send("MSG", msg, ...)
+}
