5e80673495ed9bf38274fb4dd14a7ff6d39e7f0e
Martin Maechler <maechler@r-project.org>
2016-05-02 12:44:56 +0200

    ess-version update from VERSION (forgotten 'make' before last push!)

--- lisp/ess-custom.el
+++ lisp/ess-custom.el
@@ -132,7 +132,7 @@
   :prefix "ess-")
 ;; Variables (not user-changeable)
 
-(defvar ess-version "15.09-2 patched" ;; updated by 'make'
+(defvar ess-version "16.04" ;; updated by 'make'
   "Version of ESS currently loaded.")
 
 (defvar ess-revision nil ;; set
