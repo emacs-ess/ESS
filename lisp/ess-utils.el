151b00326d7b63d039ff401de45729bfaebeb429
Lionel Henry <lionel.hry@gmail.com>
2017-11-19 22:03:27 +0100

    Only try to detect `ess-etc-directory` if not already set up
    Issue #465

--- lisp/ess-utils.el
+++ lisp/ess-utils.el
@@ -47,25 +47,24 @@
 The ESS etc directory stores various auxillary files that are useful
 for ESS, such as icons.")
 
-(defvar ess-etc-directory-list
-  '("../etc/ess/" "../etc/" "../../etc/ess/" "./etc/")
-  "List of directories, relative to `ess-lisp-directory', to search for etc.")
-
-(while (and (listp ess-etc-directory-list) (consp ess-etc-directory-list))
-  (setq ess-etc-directory
-        (expand-file-name (concat ess-lisp-directory "/"
-                                  (car ess-etc-directory-list))))
-  (if (file-directory-p ess-etc-directory)
-      (setq ess-etc-directory-list nil)
-    (setq ess-etc-directory nil)
-    (setq ess-etc-directory-list (cdr ess-etc-directory-list))
-    (when (null ess-etc-directory-list)
-      (beep 0) (beep 0)
-      (message (concat
-                "ERROR:ess-site.el:ess-etc-directory\n"
-                "Relative to ess-lisp-directory, one of the following must exist:\n"
-                "../etc/ess, ../etc, ../../etc/ess or ./etc"))
-      (sit-for 4))))
+;; Try to detect the `etc' folder only if not alread set up by distribution
+(unless ess-etc-directory
+  (let ((path-list '("../etc/ess/" "../etc/" "../../etc/ess/" "./etc/")))
+    (while (and (listp path-list) (consp path-list))
+      (setq ess-etc-directory
+            (expand-file-name (concat ess-lisp-directory "/"
+                                      (car path-list))))
+      (if (file-directory-p ess-etc-directory)
+          (setq path-list nil)
+        (setq ess-etc-directory nil)
+        (setq path-list (cdr path-list))
+        (when (null path-list)
+          (beep 0) (beep 0)
+          (message (concat
+                    "ERROR:ess-etc-directory\n"
+                    "Relative to ess-lisp-directory, one of the following must exist:\n"
+                    "../etc/ess, ../etc, ../../etc/ess or ./etc"))
+          (sit-for 4))))))
 
 (defun ess-message (format-string &rest args)
   "Shortcut for \\[message] only if `ess-show-load-messages' is non-nil."
