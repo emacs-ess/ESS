6f7499713eccba3ee3d69c609e5ef7b2f1a63416
Aaron Jacobs <atheriel@gmail.com>
2018-01-21 13:52:58 -0500

    Adds a bespoke mode for viewing local/byte-compiled R functions.

--- lisp/ess-r-xref.el
+++ lisp/ess-r-xref.el
@@ -94,13 +94,66 @@ they are loaded via byte-compiled packages."
   (let ((cmd (format "cat(\"%s <- \"); print.function(%s)\n"
                      fn fn))
         (buff (get-buffer-create
-               (format "*definition[R]:%s*" fn))))
+               (format "*definition[R]:%s*" fn)))
+        (proc ess-local-process-name))
     (with-current-buffer (ess-command cmd buff)
-      ;; TODO: This should probably have its own bespoke mode, just using the
-      ;; syntax of `r-mode', so as to avoid pullin in all of ESS's hooks.
-      (r-mode)
-      (setq buffer-read-only t))
+      (ess-r-source-mode)
+      (setq-local ess-local-process-name proc))
     (list (xref-make (format "%s" fn) (xref-make-buffer-location buff 0)))))
+
+
+;;; R Function Source Viewer Mode
+
+(defun ess-r-xref--parse-byte-compiled-p (buff)
+  "Determine whether the R function printed in BUFF is byte-compiled.
+
+This also erases the metadata once the compilation status has
+been determined."
+  (with-current-buffer buff
+    (goto-char (point-max))
+    (when (re-search-backward "^<bytecode" nil 'noerror)
+      ;; Delete the metadata, if it's there.
+      (beginning-of-line)
+      (kill-whole-line)
+      t)))
+
+(defun ess-r-xref--parse-environment (buff)
+  "Determine the environment of the R function printed in BUFF.
+
+This also erases the metadata once the environment has been
+determined."
+  (with-current-buffer buff
+    (goto-char (point-max))
+    (let* ((found (re-search-backward "^<environment: \\(.*\\)>$" nil 'noerror))
+           (env (or (when found (match-string-no-properties 1)) ".GlobalEnv")))
+      ;; Delete the metadata, if it's there.
+      (when found (beginning-of-line) (kill-whole-line))
+      env)))
+
+;; Defined in ess-r-mode.
+(defvar ess-r-syntax-table)
+
+(defvar-local ess-r-source-byte-compiled-p nil
+  "Indicates whether the R function on display is byte-compiled.")
+
+(defvar-local ess-r-source-environment nil
+  "The environment the R function on display belongs to.")
+
+(define-derived-mode ess-r-source-mode special-mode "R Source"
+  "Major mode for navigating the source of local/byte-compiled R functions."
+  :syntax-table nil
+  (setq buffer-read-only nil)
+  ;; Use R's syntax table.
+  (set-syntax-table ess-r-syntax-table)
+  ;; Move metadata to the header line.
+  (let ((compiled (ess-r-xref--parse-byte-compiled-p (current-buffer)))
+        (env (ess-r-xref--parse-environment (current-buffer))))
+    (setq-local ess-r-source-byte-compiled-p compiled)
+    (setq-local ess-r-source-environment env)
+    (setq header-line-format
+          (concat "Environment: " env (when compiled " (byte-compiled)"))))
+  (add-hook 'xref-backend-functions #'ess-r-xref-backend nil 'local)
+  (setq buffer-read-only t))
 
 (provide 'ess-r-xref)
 
