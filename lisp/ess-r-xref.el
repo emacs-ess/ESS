c4bbc3e031d20cad31d6430e517f97f4f7256382
Aaron Jacobs <atheriel@gmail.com>
2018-01-29 21:56:50 -0500

    Removes `ess-r-source-mode'.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- lisp/ess-r-xref.el
+++ lisp/ess-r-xref.el
@@ -53,8 +53,7 @@
   (inferior-ess-r-force)
   ;; Ignore non-functions.
   (when (ess-r-xref--fn-exists-p symbol)
-    (or (ess-r-xref--srcfile-xref symbol)
-        (ess-r-xref--body-xref symbol))))
+    (ess-r-xref--srcfile-xref symbol)))
 
 (cl-defmethod xref-backend-apropos ((_backend (eql ess-r)))
   ;; Not yet supported.
@@ -118,83 +117,6 @@ they are loaded via byte-compiled packages."
                           (expand-file-name fname) line 0)))))))
 
 
-;;; Local/Byte-compiled Locations
-
-(defun ess-r-xref--body-xref (symbol)
-  "Creates an xref to a buffer containing the body of the R function SYMBOL."
-  (let ((cmd (format "base::cat(\"%s <- \"); base::print.function(%s)\n"
-                     (if (listp symbol)
-                         (concat (car symbol) "::" (cadr symbol))
-                       symbol)
-                     (ess-r-xref--get-symbol symbol)))
-        (buff (get-buffer-create
-               (format "*definition[R]:%s*"
-                       (if (listp symbol)
-                           (concat (car symbol) "::" (cadr symbol))
-                         symbol))))
-        (proc ess-local-process-name))
-    (with-current-buffer (ess-command cmd buff)
-      (ess-r-source-mode)
-      (setq-local ess-local-process-name proc))
-    (list (xref-make (if (listp symbol)
-                         (concat (car symbol) "::" (cadr symbol))
-                       symbol)
-                     (xref-make-buffer-location buff 0)))))
-
-
-;;; R Function Source Viewer Mode
-
-(defun ess-r-xref--parse-byte-compiled-p (buff)
-  "Determine whether the R function printed in BUFF is byte-compiled.
-
-This also erases the metadata once the compilation status has
-been determined."
-  (with-current-buffer buff
-    (goto-char (point-max))
-    (when (re-search-backward "^<bytecode" nil 'noerror)
-      ;; Delete the metadata, if it's there.
-      (beginning-of-line)
-      (kill-whole-line)
-      t)))
-
-(defun ess-r-xref--parse-environment (buff)
-  "Determine the environment of the R function printed in BUFF.
-
-This also erases the metadata once the environment has been
-determined."
-  (with-current-buffer buff
-    (goto-char (point-max))
-    (let* ((found (re-search-backward "^<environment: \\(.*\\)>$" nil 'noerror))
-           (env (or (when found (match-string-no-properties 1)) ".GlobalEnv")))
-      ;; Delete the metadata, if it's there.
-      (when found (beginning-of-line) (kill-whole-line))
-      env)))
-
-;; Defined in ess-r-mode.
-(defvar ess-r-syntax-table)
-
-(defvar-local ess-r-source-byte-compiled-p nil
-  "Indicates whether the R function on display is byte-compiled.")
-
-(defvar-local ess-r-source-environment nil
-  "The environment the R function on display belongs to.")
-
-(define-derived-mode ess-r-source-mode special-mode "R Source"
-  "Major mode for navigating the source of local/byte-compiled R functions."
-  :syntax-table nil
-  (setq buffer-read-only nil)
-  ;; Use R's syntax table.
-  (set-syntax-table ess-r-syntax-table)
-  ;; Move metadata to the header line.
-  (let ((compiled (ess-r-xref--parse-byte-compiled-p (current-buffer)))
-        (env (ess-r-xref--parse-environment (current-buffer))))
-    (setq-local ess-r-source-byte-compiled-p compiled)
-    (setq-local ess-r-source-environment env)
-    (setq header-line-format
-          (concat "Environment: " env (when compiled " (byte-compiled)"))))
-  (add-hook 'xref-backend-functions #'ess-r-xref-backend nil 'local)
-  (setq buffer-read-only t))
-
 (provide 'ess-r-xref)
 
 ;;; ess-r-xref.el ends here
