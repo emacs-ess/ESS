64868d8ed24e3dd543258fd554553526fbd88597
Alex Branham <branham@utexas.edu>
2018-04-02 09:21:34 -0500

    dir-locals.el: Setup bug-reference variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,10 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((emacs-lisp-mode
+((nil
+  (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+  (bug-reference-url-format . "https://github.com/emacs-ess/ess/issues/%s"))
+ (emacs-lisp-mode
   ;; (coding . utf-8-unix) ;; emacs 24.3.5 gives "Coding cannot be specified by dir-locals"
   (indent-tabs-mode)
   (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark)))
