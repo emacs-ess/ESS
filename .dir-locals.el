fc4152521e3ffeecdd4a79b35d1e9870e3be06ac
Vitalie Spinu <spinuvit@gmail.com>
2018-06-16 11:16:57 +0200

    Don't require out-of-fashion double space ending of sentences

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,7 @@
 ((nil
   (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
   (bug-reference-url-format . "https://github.com/emacs-ess/ess/issues/%s")
-  (sentence-end-double-space . t))
+  (sentence-end-double-space . nil))
  (emacs-lisp-mode
   (indent-tabs-mode)
   (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark)))
