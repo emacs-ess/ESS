9a006151083784db8014d7a8ead1ab99df7b7c5a
Lionel Henry <lionel.hry@gmail.com>
2018-06-17 15:46:09 +0200

    Add support for multiple cursors in literate tests

--- test/literate/elt.R
+++ test/literate/elt.R
@@ -11,3 +11,36 @@
 
 ¶NULL
 
+
+### 2 Multiple cursors -----------------------------------------------
+
+¶NULL
+¶NA
+foo ¶bar
+
+##! (forward-word)
+##> (should (eolp))
+
+NULL¶
+NA¶
+foo bar¶
+
+##> (backward-word)
+
+¶NULL
+¶NA
+foo ¶bar
+
+
+### 3 Multiple cursors on single lines -------------------------------
+
+¶NULL ¶NULL
+¶NA ¶NA
+¶foo ¶bar
+
+##! (forward-word)
+
+NULL¶ NULL¶
+NA¶ NA¶
+foo¶ bar¶
+
