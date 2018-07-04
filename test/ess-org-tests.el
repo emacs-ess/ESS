2d1a8f35ce96b1686c876562f4bb92aa6ace370d
Vitalie Spinu <spinuvit@gmail.com>
2018-07-04 17:44:35 +0200

    Add more org-ob-R tests
      #598

--- test/ess-org-tests.el
+++ test/ess-org-tests.el
@@ -4,20 +4,40 @@
 (require 'ess-r-tests-utils)
 (require 'ob-R)
 
-(ert-deftest test-org-ob-R-output ()
+(defun test-org-R-ouput (expect input)
+  (declare (indent 1))
   (let ((proc (ess-vanila-R))
-        (org-confirm-babel-evaluate nil))
+        (org-confirm-babel-evaluate nil)
+        (ess-ask-for-ess-directory nil))
     (unwind-protect
-        (with-temp-buffer
-          (insert "
-#+BEGIN_SRC R :session :results output
-  print(\"hello\")
-#+END_SRC
-")
+        (with-current-buffer (get-buffer-create "*ess-org-test*")
+          (erase-buffer)
+          (insert input)
           (org-mode)
           (goto-char (point-min))
-          (forward-line 2)
+          (forward-line 1)
           (org-ctrl-c-ctrl-c)
           (goto-char (point-max))
-          (should (looking-back "\"hello\"\n+")))
+          (should (re-search-backward expect nil t)))
       (kill-process proc))))
+
+(ert-deftest test-org-ob-R-output ()
+  (test-org-R-ouput "hello"
+    "#+BEGIN_SRC R :results output\n  \"hello\"\n#+END_SRC"))
+
+(ert-deftest test-org-ob-R-session-output ()
+  (test-org-R-ouput "hello"
+    "#+BEGIN_SRC R :session :results output\n  \"hello\"\n#+END_SRC"))
+
+(ert-deftest test-org-ob-R-value ()
+  (test-org-R-ouput "hello"
+    "#+BEGIN_SRC R :results value\n  \"hello\"\n#+END_SRC"))
+
+(ert-deftest test-org-ob-R-session-value ()
+  (test-org-R-ouput "hello"
+    "#+BEGIN_SRC R :session :results value\n  \"hello\"\n#+END_SRC"))
+
+(ert-deftest test-org-ob-R-data-frame ()
+  (test-org-R-ouput "| 3 | c |"
+    "#+BEGIN_SRC R :session :results value :colnames yes\n  data.frame(x=1:3, y=c(\"a\",\"b\",\"c\"))\n#+END_SRC"))
+
