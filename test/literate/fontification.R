b59bb7258d6e888ef52461b8da3d087057ffe32d
Lionel Henry <lionel.hry@gmail.com>
2018-07-29 12:45:29 +0200

    Use multiple cursors in fontification tests

--- test/literate/fontification.R
+++ test/literate/fontification.R
@@ -3,136 +3,116 @@
 
 ### 1 Bare function-like keywords are not fontified ------------------
 
-¶while for if switch function return on.exit stop
-tryCatch withRestarts invokeRestart recover browser
+¶while ¶for ¶if ¶switch ¶function ¶return ¶on.exit ¶stop
+¶tryCatch ¶withRestarts ¶invokeRestart ¶recover ¶browser
 
-##! (while (not (eolp))
-##>   (when (>= emacs-major-version 25)
-##>     (should (not (face-at-point))))
-##>   (forward-sexp)
-##>   (ignore-errors (forward-char)))
+##! (when (>= emacs-major-version 25)
+##>   (should (not (face-at-point))))
 
-while for if switch function return on.exit stop
-tryCatch withRestarts invokeRestart recover browser¶
+¶while ¶for ¶if ¶switch ¶function ¶return ¶on.exit ¶stop
+¶tryCatch ¶withRestarts ¶invokeRestart ¶recover ¶browser
 
 
 ### 1b Bare function-like weak keywords are not fontified ------------
 
-¶message warning signalCondition withCallingHandlers
+¶message ¶warning ¶signalCondition ¶withCallingHandlers
 
-##! (while (not (eolp))
-##>   (should (not (face-at-point)))
-##>   (forward-sexp)
-##>   (ignore-errors (forward-char)))
+##! (should (not (face-at-point)))
 
-message warning signalCondition withCallingHandlers¶
+¶message ¶warning ¶signalCondition ¶withCallingHandlers
 
 
 ### 2 Function-like keywords are fontified ---------------------------
 
-¶while() for() if() function()
+¶while() ¶for() ¶if() ¶function()
 
-##! (while (not (eolp))
-##>   (should (eq (face-at-point) 'ess-keyword-face))
-##>   (ess-forward-sexp)
-##>   (should (not (face-at-point)))
-##>   (ignore-errors (forward-char 3)))
+##! (should (eq (face-at-point) 'ess-keyword-face))
+##> (ess-forward-sexp)
+##> (should (not (face-at-point)))
 
-while() for() if() function()¶
+while¶() for¶() if¶() function¶()
 
 
 ### 2b Function-like control flow keywords are fontified -------------
 
-¶switch() return() on.exit() stop() tryCatch()
-withRestarts() invokeRestart() recover() browser()
+¶switch() ¶return() ¶on.exit() ¶stop() ¶tryCatch()
+¶withRestarts() ¶invokeRestart() ¶recover() ¶browser()
 
-##! (while (not (eolp))
-##>   (should (eq (face-at-point) 'ess-r-control-flow-keyword-face))
-##>   (ess-forward-sexp)
-##>   (should (not (face-at-point)))
-##>   (ignore-errors (forward-char 3)))
+##! (should (eq (face-at-point) 'ess-r-control-flow-keyword-face))
+##> (ess-forward-sexp)
+##> (should (not (face-at-point)))
 
-switch() return() on.exit() stop() tryCatch()
-withRestarts() invokeRestart() recover() browser()¶
+switch¶() return¶() on.exit¶() stop¶() tryCatch¶()
+withRestarts¶() invokeRestart¶() recover¶() browser¶()
 
 
 ### 2c Function-like signal keywords are fontified ---------------------
 
-¶message() warning() signalCondition() withCallingHandlers()
+¶message() ¶warning() ¶signalCondition() ¶withCallingHandlers()
 
-##! (while (not (eolp))
-##>   (should (eq (face-at-point) 'ess-r-signal-keyword-face))
-##>   (ess-forward-sexp)
-##>   (should (not (face-at-point)))
-##>   (ignore-errors (forward-char 3)))
+##! (should (eq (face-at-point) 'ess-r-signal-keyword-face))
+##> (ess-forward-sexp)
+##> (should (not (face-at-point)))
 
-message() warning() signalCondition() withCallingHandlers()¶
+message¶() warning¶() signalCondition¶() withCallingHandlers¶()
 
 
 ### 3 Simple keywords are always fontified ---------------------------
 
-¶in else break next repeat
+¶in ¶else ¶break ¶next ¶repeat
 
-##! (while (not (eolp))
-##>   (should (eq (face-at-point) 'ess-keyword-face))
-##>   (forward-word)
-##>   (ignore-errors (forward-char)))
+##! (should (eq (face-at-point) 'ess-keyword-face))
 
-in else break next repeat¶
+¶in ¶else ¶break ¶next ¶repeat
 
 
 ### 4 Search list modifiers are not fontified if not in function position
 
-¶library attach detach source require
+¶library ¶attach ¶detach ¶source ¶require
 
-##! (while (not (eolp))
-##>   (when (>= emacs-major-version 25)
-##>     (should (not (face-at-point))))
-##>   (forward-word)
-##>   (ignore-errors (forward-char)))
+##! (when (>= emacs-major-version 25)
+##>   (should (not (face-at-point))))
 
-library attach detach source require¶
+¶library ¶attach ¶detach ¶source ¶require
 
 
 ### 5 Search list modifiers are fontified if in function position ----
 
-¶library() attach() detach() source() require()
+¶library() ¶attach() ¶detach() ¶source() ¶require()
 
-##! (while (not (eolp))
-##>   (should (eq (face-at-point) 'ess-modifiers-face))
-##>   (forward-word)
-##>   (should (not (face-at-point)))
-##>   (ignore-errors (forward-char 3)))
+##! (should (eq (face-at-point) 'ess-modifiers-face))
+##> (forward-word)
+##> (should (not (face-at-point)))
 
-library() attach() detach() source() require()¶
+library¶() attach¶() detach¶() source¶() require¶()
 
 
 ### 6 Assignment operators are fontified -----------------------------
 
-foo¶ <- foo <<- foo -> foo ->> foo
+foo¶ <- foo¶ <<- foo¶ -> foo¶ ->> foo
 
-##! (while (not (eolp))
-##>   (should (not (face-at-point)))
-##>   (forward-char)
-##>   (should (eq (face-at-point) 'ess-assignment-face))
-##>   (skip-syntax-forward ".")
-##>   (should (not (face-at-point)))
-##>   (ignore-errors (forward-word)))
+##! (should (not (face-at-point)))
+##> (forward-char)
 
-foo <- foo <<- foo -> foo ->> foo¶
+foo ¶<- foo ¶<<- foo ¶-> foo ¶->> foo
+
+##> (should (eq (face-at-point) 'ess-assignment-face))
+##> (skip-syntax-forward ".")
+
+foo <-¶ foo <<-¶ foo ->¶ foo ->>¶ foo
+
+##> (should (not (face-at-point)))
+
+foo <-¶ foo <<-¶ foo ->¶ foo ->>¶ foo
 
 
 ### 7 Constants are fontified ----------------------------------------
 
-¶TRUE FALSE NA NULL Inf NaN
-NA_integer_ NA_real_ NA_complex_ NA_character_
+¶TRUE ¶FALSE ¶NA ¶NULL ¶Inf ¶NaN
+¶NA_integer_ ¶NA_real_ ¶NA_complex_ ¶NA_character_
 
-##! (while (not (eolp))
-##>   (should (eq (face-at-point) 'ess-constant-face))
-##>   (ignore-errors
-##>     (ess-forward-sexp)
-##>     (forward-char)))
+##! (should (eq (face-at-point) 'ess-constant-face))
 
-TRUE FALSE NA NULL Inf NaN
-NA_integer_ NA_real_ NA_complex_ NA_character_¶
+¶TRUE ¶FALSE ¶NA ¶NULL ¶Inf ¶NaN
+¶NA_integer_ ¶NA_real_ ¶NA_complex_ ¶NA_character_
 
