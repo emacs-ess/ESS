09b42cbe6096c2a9562ef68a289779141a6bd823
Vitalie Spinu <spinuvit@gmail.com>
2018-09-10 20:55:51 +0200

    Update package.el short doc

--- lisp/ess.el
+++ lisp/ess.el
@@ -34,7 +34,10 @@
 ;;
 ;;; Commentary:
 ;;
-;; Header of this file is used for package.el
+;; Emacs Speaks Statistics (ESS) is a package designed to support editing of
+;; scripts and interaction with various statistical analysis programs such as R,
+;; S-Plus, SAS, Stata and OpenBUGS/JAGS. For more details please visit ESS home
+;; page at https://ess.r-project.org/
 ;;
 ;;; Code:
 
