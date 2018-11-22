1c600264753128c55dac3528c1fa9661b35249e6
dickmao <dick.r.chiang@gmail.com>
2018-11-21 19:22:45 -0500

    VERSION cannot be 18.10.2-git... else you get weird tarball directories like ess-18.10.2-git-git

--- lisp/ess.el
+++ lisp/ess.el
@@ -16,7 +16,7 @@
 ;; Maintainer: ESS Core Team <ESS-core@r-project.org>
 ;; Copyright (C) 1997-2018 ESS Core Team <ESS-core@r-project.org>
 ;; Created: 7 Jan 1994
-;; Version: 18.10.2-git
+;; Version: 18.10.2
 ;; Package-Requires: ((julia-mode "0.3"))
 ;; ESSR-Version: 1.4
 ;;
@@ -693,7 +693,7 @@ Function defined using `ess-define-runner'."
 ;; Variables (not user-changeable)
 
 ;; updated by 'make'!
-(defconst ess-version "18.10.2-git"
+(defconst ess-version "18.10.2"
   "Version of ESS currently loaded.")
 
 ;; updated by 'make'!
