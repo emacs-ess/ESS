9cc5520e1998d03f5dec0fbb1fe71b7cdec38b65
Alex Branham <alex.branham@gmail.com>
2021-01-06 06:41:20 -0500

    Add required when to obsolete function alias
    Closes #1085

--- lisp/ess-r-package.el
+++ lisp/ess-r-package.el
@@ -577,7 +577,7 @@ package mode. Use this function if state of the buffer such as
   (error "As of ESS 16.04, `ess-developer' is deprecated. Use `ess-r-set-evaluation-env' instead"))
 
 (defalias 'ess-toggle-developer 'ess-developer)
-(define-obsolete-function-alias 'ess-r-devtools-check-package-buildwin 'ess-r-devtools-check-with-winbuilder)
+(define-obsolete-function-alias 'ess-r-devtools-check-package-buildwin 'ess-r-devtools-check-with-winbuilder "18.04")
 (define-obsolete-function-alias 'ess-r-devtools-ask 'ess-r-devtools-execute-command "18.04")
 
 (make-obsolete-variable 'ess-developer "Please use `ess-developer-select-package' and `ess-r-set-evaluation-env' instead." "16.04")
