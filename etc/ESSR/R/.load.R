a83206ab65ed5f0b1710de69241938f2fc7e3d25
Vitalie Spinu <spinuvit@gmail.com>
2021-03-03 22:22:18 +0100

    Simplify #1107 a bit

--- etc/ESSR/R/.load.R
+++ etc/ESSR/R/.load.R
@@ -2,44 +2,32 @@
 ## inferior-ess-r-load-ESSR--local.
 ## Do not use _ in names, nor :: as they cannot be parsed in old R versions
 
+## obtain the R version number as a string
+.ess.ESSR.get.rver <- function() {
+    if(exists("getRversion", mode="function")) getRversion()
+    else paste(R.version$major, R.version$minor, sep=".")
+}
+
 ## load .base.R and all other files into ESSR environment; then attach ESSR
-.ess.load.ESSR <- function(dir) {
+.ess.ESSR.load <- function(dir) {
 
-    Rver <- .ess.load.ESSR.get.rver()
-    oldR <- .ess.load.ESSR.check.oldr(Rver)
-
-    ESSR <- .ess.load.ESSR.create.env(Rver, oldR)
-    ESSR <- .ess.load.ESSR.source.files(ESSR, dir, oldR)
-    .ess.load.ESSR.attach.env(ESSR, Rver, oldR)
+    Rver <- .ess.ESSR.get.rver()
+    ESSR <- .ess.ESSR.create.env(Rver)
+    .ess.ESSR.source.files(ESSR, dir, Rver)
+    .ess.ESSR.attach.env(ESSR, Rver)
 
     ## BUILDESSR needs this:
     invisible(ESSR)
 }
 
-
-## obtain the R version number as a string
-.ess.load.ESSR.get.rver <- function() {
-    if(exists("getRversion", mode="function")) getRversion()
-    else paste(R.version$major, R.version$minor, sep=".")
-}
-
-
-## check whether R is on or before an old version (certain functions that are
-## relevant to the ESSR load process change after this version)
-.ess.load.ESSR.check.oldr <- function(Rver) {
-    Rver <= "1.3.0"
-}
-
-
-## create the ESSR evironment and add R and ESSR version information to it
-.ess.load.ESSR.create.env <- function(Rver, oldR) {
+.ess.ESSR.create.env <- function(Rver) {
 
     ESSR <-
-        if(oldR) ## really old library() revert order a bit
+        if (Rver <= "1.3.0") ## really old library() revert order a bit
             attach(NULL, name = "ESSR")
         else if(length(nn <- names(formals(new.env))) && any(nn == "parent"))
             new.env(parent =
-                        if(Rver >= "1.9.0") getNamespace("utils")
+                        if (Rver >= "1.9.0") getNamespace("utils")
                         else .BaseNamespaceEnv)
         else
             new.env()
@@ -53,11 +41,10 @@
     ESSR
 }
 
-
 ## attempt to source the files in the ESSR directory and place the resulting R
 ## objects into ESSR environment; the updated version of the environment is
 ## returned
-.ess.load.ESSR.source.files <- function(ESSR, dir, oldR) {
+.ess.ESSR.source.files <- function(ESSR, dir, Rver) {
 
     .source <-
         if(any("keep.source" == names(formals(sys.source))))
@@ -69,22 +56,23 @@
     try(.source(paste(dir,'/.basic.R', sep = ""), envir = ESSR, keep.source = FALSE))
 
     ## all others try(*) as it will fail in old R
-    if(!oldR) # no sense if(oldR)
-        for( f in dir(dir, pattern='\\.R$', full.names=TRUE) )
+    if (Rver > "1.3.0") { # no sense if very old R
+        for (f in dir(dir, pattern='\\.R$', full.names=TRUE))
             try(.source(f, envir = ESSR, keep.source = FALSE))
+    }
 
     ESSR
 }
 
 
 ## attach the ESSR environment to the search path
-.ess.load.ESSR.attach.env <- function(ESSR, Rver, oldR) {
-    if(Rver >= "2.4.0")
+.ess.ESSR.attach.env <- function(ESSR, Rver) {
+    if (Rver >= "2.4.0")
         attach(ESSR)
-    else if(!oldR) { ## borrow from older library()
+    else if (Rver > "1.3.0") { # borrow from older library()
         e <- attach(NULL, name = "ESSR")
         .Internal(lib.fixup(ESSR, e))
-    } else { ## if(oldR), use as in that old library():
+    } else { # if old R, use as in that old library():
         .Internal(lib.fixup(ESSR, .GlobalEnv))
     }
 }
@@ -94,14 +82,12 @@
 ## environment; then remove the original objects and attach ESSR
 .ess.collect.ESSR.objects <- function() {
 
-    Rver <- .ess.load.ESSR.get.rver()
-    oldR <- .ess.load.ESSR.check.oldr(Rver)
-
-    ESSR <- .ess.load.ESSR.create.env(Rver, oldR)
+    Rver <- .ess.ESSR.get.rver()
+    ESSR <- .ess.ESSR.create.env(Rver)
     essr.nms <- grep('\\.(ess|ESS)', ls(.GlobalEnv, all.names = TRUE), value = TRUE)
     for (nm in essr.nms) {
         assign(nm, get(nm, pos = .GlobalEnv), envir = ESSR)
     }
-    .ess.load.ESSR.attach.env(ESSR, Rver, oldR)
+    .ess.ESSR.attach.env(ESSR, Rver)
     rm(list = essr.nms, pos = .GlobalEnv)
 }
--- lisp/ess-r-mode.el
+++ lisp/ess-r-mode.el
@@ -1398,28 +1398,28 @@ process."
     (cond
      ((file-remote-p (ess-get-process-variable 'default-directory))
       (if (eq ess-r-fetch-ESSR-on-remotes t)
-          (ess-r--fetch-ESSR-or-fallback-load-remote)
+          (or (ess-r--fetch-ESSR-remote)
+              (ess-r--load-ESSR-remote))
         (ess-r--load-ESSR-remote)))
      ((and (bound-and-true-p ess-remote))
       ;; NB: With ess-remote we send by chunks because sending large sources is
       ;; fragile
       (if ess-r-fetch-ESSR-on-remotes
-          (ess-r--fetch-ESSR-or-fallback-load-remote t)
+          (or (ess-r--fetch-ESSR-remote)
+              (ess-r--load-ESSR-remote t))
         (ess-r--load-ESSR-remote t)))
      (t (ess-r--load-ESSR-local)))))
 
 (defun ess-r--load-ESSR-local ()
-  "Load ESSR functionality into a local process.
-Sources the .load.R file in the R process, the contents of which
-are a single function named .ess.load.ESSR function. The
-.ess.load.ESSR function is then invoked in the R runtime
-environment which has the effect of sourcing all of the contents
-of the ESSR/R directory into the ESSR environment and attaching
-the environment to the search path."
+  "Load ESSR into a local process.
+Source the etc/ESSR/.load.R file into the R process. The
+.ess.ESSR.load function sources all of the contents of the
+etc/ESSR/R directory into the ESSR environment and attaches the
+environment to the search path."
   (let* ((src-dir (expand-file-name "ESSR/R" ess-etc-directory))
          (cmd (format "local({
                           source('%s/.load.R', local=TRUE) #define load.ESSR
-                          .ess.load.ESSR('%s')
+                          .ess.ESSR.load('%s')
                       })\n"
                       src-dir src-dir)))
     (with-current-buffer (ess-command cmd nil nil nil nil nil nil 5)
@@ -1428,13 +1428,13 @@ the environment to the search path."
           (message (format "Messages while loading ESSR: %s" msg)))))))
 
 (defun ess-r--load-ESSR-remote (&optional chunked)
-  "Load ESSR functionality into a remote process through the process connection.
-Sends the contents of the ESSR/R directory to the remote process
-through the process connection, resulting in the ESSR objects
-being created in the global environment. Then as a second step
-the .essr.load.ESSR function moves the R objects into the ESSR
-environment and attaches it to the search path."
-  (ess-command (format ".ess.ESSRversion <- '%s'\n" essr-version))
+  "Load ESSR into a remote process through the process connection.
+Send the contents of the etc/ESSR/R directory to the remote
+process through the process connection file by file. Then,
+collect all the objects into an ESSR environment and attach to
+the search path. If CHUNKED is non-nil, split each file by 
+separators and send chunk by chunk."
+  (ess-command (format ".ess.ESSRversion <<- '%s'\n" essr-version))
   (with-temp-message "Loading ESSR into remote ..."
     (let ((src-dir (expand-file-name "ESSR/R" ess-etc-directory)))
       (dolist (file (directory-files src-dir t "\\.R\\'"))
@@ -1442,26 +1442,20 @@ environment and attaches it to the search path."
       (ess-command ".ess.collect.ESSR.objects()\n" nil nil nil nil nil nil 5))))
 
 (defun ess-r--fetch-ESSR-remote ()
-  "Load ESSR functionality into a remote process through a GitHub download.
+  "Load ESSR into a remote process through a GitHub download.
 Downloads a file with a serialized version of the R ESSR
-environment from GitHub and reads it into R and attaches the
-environment to the search path. If the file already exists on
-disk from a previous download then the download step is omitted.
-This function returns t if the ESSR load is successful, and nil
-otherwise."
+environment from GitHub and attaches it to the search path. If
+the file already exists on disk from a previous download then the
+download step is omitted. This function returns t if the ESSR
+load is successful, and nil otherwise."
   (let ((loader (ess-file-content (expand-file-name "ESSR/LOADREMOTE" ess-etc-directory))))
-    (or (with-temp-message "Loading ESSR on the remote ..."
+    (or (with-temp-message "Fetching and loading ESSR into the remote ..."
           (ess-boolean-command (format loader essr-version)))
         (let* ((errmsg (with-current-buffer " *ess-command-output*" (buffer-string)))
                (src-dir (expand-file-name "ESSR/R" ess-etc-directory))
                (files (directory-files src-dir t "\\.R\\'")))
           (message (format "Couldn't load or download ESSR.rds on the remote.\n Error: %s\n Injecting local copy of ESSR." errmsg))
           nil))))
-
-(defun ess-r--fetch-ESSR-or-fallback-load-remote (&optional chunked)
-  "First try ESSR GitHub fetch, otherwise use the process connection."
-  (or (ess-r--fetch-ESSR-remote)
-      (ess-r--load-ESSR-remote chunked)))
 
 (cl-defmethod ess-quit--override (arg &context (ess-dialect "R"))
   "With ARG, do not offer to save the workspace."
